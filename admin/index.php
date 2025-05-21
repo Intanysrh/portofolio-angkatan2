2eea9629590b2bb78a95798d52f3e36d74a2731c
Intanysrh <intanysrh@gmail.com>
2025-05-21 15:37:10 +0700

    koneksi data base

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+include "config/koneksi.php";
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = sha1($_POST['password']);
+
+    $query = mysqli_query($config, "SELECT * FROM users WHERE email = '$email' && password='$password'");
+
+    if (mysqli_num_rows($query) > 0) {
+        $row = mysqli_fetch_assoc($query);
+        $_SESSION['NAME'] = $row['name'];
+        $_SESSION['ID_USER'] = $row['id'];
+        header("location:dashboard.php");
+    } else {
+        header("location:index.php?error=login");
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,36 +32,36 @@
 
 <body>
 
-<div class="wrapper">
-    <div class="login-form mt-5">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Login Form
-                        </div>
-                        <div class="card-body">
-                            <form action="" method="post">
-                                <div class="mb-3">
-                                    <label for="" class="form-label">Email</label>
-                                    <input type="email" class="form-control" name="email" placeholder="ex:admin@gmail.com">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="" class="form-label">Password</label>
-                                    <input type="password" class="form-control" name="email" placeholder="Masukan Password Anda">
-                                </div>
-                                <div class="mb-3">
-                                    <button type="submit" class="btn btn-primary">Login</button>
-                                </div>
-                            </form>
+    <div class="wrapper">
+        <div class="login-form mt-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">
+                                Login Form
+                            </div>
+                            <div class="card-body">
+                                <form action="" method="post">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Email</label>
+                                        <input type="email" class="form-control" name="email" id="email" placeholder="ex:admin@gmail.com">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Password</label>
+                                        <input type="password" id="password" class="form-control" name="password" placeholder="Masukan Password Anda">
+                                    </div>
+                                    <div class="mb-3">
+                                        <button type="submit" class="btn btn-primary">Login</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
 </body>
--- admin/user.php
+++ admin/user.php
@@ -1,3 +1,19 @@
+<?php
+include 'config/koneksi.php';
+
+// munculkan/pilih semua data dari tabel user urutkan dari yg terbesar ke yg terkecil
+
+$query = mysqli_query($config, "SELECT * FROM users ORDER BY id DESC");
+$row = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $queryDelete = mysqli_query($config, "DELETE FROM users WHERE id='$id'");
+    header("location:user.php?hapus=berhasil");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,44 +22,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form | Portofolio Intan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div class="wrapper">
-        <header class="shadow">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Page
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="user.php">User</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        <?php include 'inc/header.php'; ?>
     </div>
 
     <div class="content mt-5">
@@ -69,16 +52,19 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Intan</td>
-                                            <td>intan@gamil.com</td>
-                                            <td>
-                                                <a href="" class="btn btn-success btn-sm">Edit</a>
-                                                <a onclick="return confirm (`Are you sure?`)"
-                                                    href="" class="btn btn-warning btn-sm">Delete</a>
-                                            </td>
-                                        </tr>
+                                        <?php $i = 1;
+                                        foreach ($row as $key => $data): ?>
+                                            <tr>
+                                                <td><?= $key + 1 ?></td>
+                                                <td><?= $data['name'] ?></td>
+                                                <td><?= $data['email'] ?></td>
+                                                <td>
+                                                    <a href="tambah-user.php?edit=<?php echo $data['id'] ?>" class="btn btn-success btn-sm">Edit</a>
+                                                    <a onclick="return confirm (`Are you sure?`)"
+                                                        href="user.php?delete=<?php echo $data['id'] ?>" class="btn btn-warning btn-sm">Delete</a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -89,8 +75,7 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 
 </html>
